acc0336cdbd3790f77f662d5760a1dfaa59525db
namnguyen <namnguyenktxd@gmail.com>
2023-04-02 18:26:53 +0700

    update responsive page detail

--- src/pages/books/[id].tsx
+++ src/pages/books/[id].tsx
@@ -1,7 +1,7 @@
 import { Book } from '@/models/books';
 import { get } from '@/services/api';
 import { getRatingValue } from '@/utils/rating';
-import { Button, Grid, Rating, Typography } from '@mui/material';
+import { Button, Container, Grid, Rating, Typography } from '@mui/material';
 
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
@@ -32,21 +32,23 @@ export default function BookDetail() {
         const bookData: Book = JSON.parse(JSON.stringify(res));
         setBook(bookData);
       })
-      .catch((e) => {});
+      .catch((e) => { });
   }, [id, router.isReady]);
   return (
-    <Grid container>
-      <Grid item xs={8}>
-        <img alt={book.name} src={book?.avatar} />
-      </Grid>
-      <Grid item xs={4}>
-        <h1>{book?.name}</h1>
-        <Rating value={getRatingValue(book?.rating)}></Rating>
-        <Typography>{book?.description}</Typography>
+    <Container maxWidth='lg'>
+      <Grid container>
+        <Grid item xs={12} md={12} lg={8}>
+          <img alt={book.name} src={book?.avatar} />
+        </Grid>
+        <Grid item xs={12} md={12} lg={4}>
+          <h1>{book?.name}</h1>
+          <Rating value={getRatingValue(book?.rating)}></Rating>
+          <Typography>{book?.description}</Typography>
 
-        <Button>Add to cart</Button>
-        <Button>Like</Button>
+          <Button>Add to cart</Button>
+          <Button>Like</Button>
+        </Grid>
       </Grid>
-    </Grid>
+    </Container>
   );
 }
